9c29452189de451955b2ceedd2aafe25f63a5dd1
Jakke Korpelainen <jakke.korpelainen@gmail.com>
2017-05-03 14:32:15 +0300

    Enabled vue-resource

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import Router from 'vue-router'
 import FillariMap from '@/components/FillariMap'
 
 Vue.use(Router)
+Vue.use(VueResource)
 
 export default new Router({
   routes: [
