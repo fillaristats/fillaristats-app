9f088bdce847a42aa7d6f671bb1014eb9c8e0cf0
Jani Luukko <jj.luukko@gmail.com>
2017-05-08 20:17:40 +0300

    Connected vue with backend, add markers from /api/stations

--- src/components/FillariMap.vue
+++ src/components/FillariMap.vue
@@ -2,7 +2,7 @@
   <div id="map">
     <v-map :zoom=13 :center="[60.192059, 24.945831]">
       <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
-      <v-marker :lat-lng="[60.192059, 24.945831]"></v-marker>
+      <v-marker v-on:l-click="selectStation(station)" v-for="station in stations" :key="station.stationId" :lat-lng="[station.lat, station.lon]"></v-marker>
     </v-map>
   </div>
 </template>
@@ -16,6 +16,24 @@ export default {
     'v-map': Vue2Leaflet.Map,
     'v-tilelayer': Vue2Leaflet.TileLayer,
     'v-marker': Vue2Leaflet.Marker
+  },
+  data () {
+    return {
+      stations: []
+    }
+  },
+  methods: {
+    fetchStations () {
+      this.$http.get('http://127.0.0.1:3000/api/stations').then((data) => {
+        this.stations = data.body
+      })
+    },
+    selectStation (station) {
+      alert(station.name)
+    }
+  },
+  mounted () {
+    this.fetchStations()
   }
 }
 </script>
